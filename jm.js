27484f8b046f7abddcc4c73b120a5b4bf67a4dfa
AriaArcadia <107249147+AriaArcadia@users.noreply.github.com>
2026-03-20 22:12:41 +0800

    Update jm.js

--- jm.js
+++ jm.js
@@ -818,7 +818,7 @@ class JM extends ComicSource {
             let updateDate = `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`;
 
             return new ComicDetails({
-                title: data.item,
+                title: data.name ?? "N/A",
                 subtitle: data.author ?? "N/A",
                 cover: this.getCoverUrl(id),
                 description: data.description,
